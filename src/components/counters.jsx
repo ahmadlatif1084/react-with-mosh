70998945f610a1135f426506f0c1588dbd29f085
rcgdeveloper1 <49670219+rcgdeveloper1@users.noreply.github.com>
2019-12-16 18:24:45 +0500

    working last part

---
+++ src/components/counters.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import Counter from './counter';
+class Counters extends Component {
+    state = { 
+        counters:[
+            {id:1,value:4},
+            {id:2,value:0},
+            {id:3,value:0},
+            {id:4,value:0},
+            {id:5,value:0},
+        ]
+     };
+     handleIncrement = counter => {
+       const counters = [...this.state.counters];
+       counters[0].value++;
+       console.log(this.state.counters[0]);
+     };
+     handleDelete = (counterId) =>{
+         const counters = this.state.counters.filter(c => c.id !== counterId);
+         this.setState({counters});
+     };
+     handleReset = () => {
+      const counters=this.state.counters.map(c =>{
+        c.value =0;
+        return c;
+        });
+     };
+    render() {
+        return (
+          <div>
+              <button onClick={ this.handleReset } className="btn btn-primary btn-sm m-2">Reset</button>
+           {this.state.counters.map(counter=> 
+            <Counter 
+            key={counter.id}
+            onDelete={this.handleDelete}
+            onIncrement={this.handleIncrement}
+            value={counter.value}
+            counter={counter} 
+            id={counter.id}>
+                 <h4>Counter #{counter.id}</h4>
+                </Counter>
+            )}
+        </div>);
+    }
+}
+ 
+export default Counters;
